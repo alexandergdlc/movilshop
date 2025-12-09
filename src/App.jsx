16cd67b7cadac0fb9367fa7a80a4ab63b6590ea7
ALEXANDER RUBEN GOMEZ DE LA CRUZ <alexander.gomez.27@unsch.edu.pe>
2025-12-08 20:12:26 -0500

    Fix: Add missing routes and Supabase env check

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import ProductDetail from './pages/ProductDetail';
 import CategoryPage from './pages/CategoryPage';
 import SearchResults from './pages/SearchResults';
+import Login from './pages/Login';
+import Checkout from './pages/Checkout';
 import Cart from './components/Cart';
 import { CartProvider } from './context/CartContext';
 
@@ -18,7 +20,8 @@ function App() {
             <Route path="/product/:id" element={<ProductDetail />} />
             <Route path="/category/:categoryName" element={<CategoryPage />} />
             <Route path="/search" element={<SearchResults />} />
-            {/* Add more routes here */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/checkout" element={<Checkout />} />
           </Routes>
           <Cart />
         </Layout>
